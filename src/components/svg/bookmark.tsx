5acaa8a43f1ce61b01e047911faac8b0ab18a901
Tika <captika@outlook.com>
2021-04-07 14:09:45 +0100

    Add SVG icons

---
+++ src/components/svg/bookmark.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { IconProps } from "../icons";
+
+export function Moon({ onClick, className }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={className}
+      onClick={onClick}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+      />
+    </svg>
+  );
+}
---
+++ src/components/svg/heart.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { IconProps } from "../icons";
+
+export function Moon({ onClick, className }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={className}
+      onClick={onClick}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+      />
+    </svg>
+  );
+}

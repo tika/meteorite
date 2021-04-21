57980c2f7a1b2a9f609a18107395cca4ebfbba38
tika <captika@outlook.com>
2021-04-21 18:12:06 +0100

    feat: Search input

--- src/components/pages/right.tsx
+++ src/components/pages/right.tsx
@@ -1,11 +1,23 @@
+import { Search } from "@components/svg/search";
 import React from "react";
 
 export function Right() {
-    return (
-        <div className="sm:flex justify-center w-full hidden sm:col-span-3">
-            <div className="fixed">
-                <h1>right</h1>
-            </div>
+  return (
+    <div className="sm:flex justify-center w-full hidden sm:col-span-3 py-8">
+      <div className="fixed">
+        <div className="relative rounded-full shadow-sm">
+          <div className="absolute inset-y-0 left-0 pl-6 flex items-center pointer-events-none">
+            <Search className="w-5 h-5 text-gray-800" />
+          </div>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className="focus:ring-indigo-500 py-3 focus:border-indigo-500 block w-72 pl-16 pr-4 sm:text-sm border-gray-300 bg-gray-100 rounded-full"
+            placeholder="Search everywhere"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }

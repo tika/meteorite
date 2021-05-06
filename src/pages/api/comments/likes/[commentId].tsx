1f806828c52faad715a5039044d4374ca01376a7
tika <captika@outlook.com>
2021-05-06 14:10:29 +0100

    feat: Comment liking routes

---
+++ src/pages/api/comments/likes/[commentId].tsx
@@ -0,0 +1,85 @@
+import { createEndpoint } from "@app/endpoint";
+import { NotFound } from "@app/exceptions";
+import { JWT } from "@app/jwt";
+import { prisma } from "@app/prisma";
+
+export default createEndpoint({
+  GET: async (req, res) => {
+    const commentId = req.query.commentId as string;
+
+    const comment = await prisma.comment.findFirst({
+      where: { id: commentId },
+      include: { likedBy: true },
+    });
+
+    if (!comment) {
+      throw new NotFound("comment");
+    }
+
+    const safeUsers = comment.likedBy.map((user) => {
+      const { email, password, ...rest } = user;
+      return rest;
+    });
+
+    res.json({ likedBy: comment });
+  },
+  DELETE: async (req, res) => {
+    const user = JWT.parseRequest(req);
+    const commentId = req.query.commentId as string;
+
+    if (!user) {
+      throw new NotFound("user");
+    }
+
+    const comment = await prisma.comment.findFirst({
+      where: { id: commentId },
+    });
+
+    if (!comment) {
+      throw new NotFound("comment");
+    }
+
+    await prisma.comment.update({
+      where: { id: commentId },
+      data: {
+        likedBy: {
+          disconnect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    res.json({ success: true });
+  },
+  PUT: async (req, res) => {
+    const user = JWT.parseRequest(req);
+
+    if (!user) {
+      throw new Error("Unknown User");
+    }
+
+    const commentId = req.query.commentId as string;
+
+    const comment = await prisma.comment.findFirst({
+      where: { id: commentId },
+    });
+
+    if (!comment) {
+      throw new NotFound("comment");
+    }
+
+    await prisma.comment.update({
+      where: { id: commentId },
+      data: {
+        likedBy: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    res.json({ success: true });
+  },
+});
